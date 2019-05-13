e76c3aa24ff380d24b6da5d3d81d5f0061d2e40d
ymdi <ymmotodaisuke@gmail.com>
2019-05-13 12:24:49 +0900

    update : update index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,22 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+      <h1>Welcome to Scynchro.</h1>
+      <p>
+        Scynchroは、動画を友達と一緒に見るためのアプリケーションです！
+      </p>
       <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
-        >
+        <v-card-title class="headline">ルームに入る</v-card-title>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
+          <v-form v-model="valid">
+            <v-text-field v-model="name" label="Name"></v-text-field>
+            <v-text-field
+              v-model="roomId"
+              label="Room ID(default : 1)"
+            ></v-text-field>
+            <v-btn block color="primary">Enter</v-btn>
+          </v-form>
+          <hr class="my-3" />
           <p>
             Find a bug? Report it on the github
             <a
@@ -34,38 +26,22 @@
               >issue board</a
             >.
           </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  layout: 'layout',
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  components: {},
+  data() {
+    return {
+      valid: false,
+      name: '',
+      roomId: ''
+    }
+  },
+  layout: 'layout'
 }
 </script>
